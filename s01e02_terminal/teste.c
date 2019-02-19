aeafd0ebe8cf50bea435245776588ff6e3556648
david sena oliveira <sena.ufc@gmail.com>
2019-02-19 09:59:22 -0300

    up

---
+++ s01e02_terminal/teste.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    puts("Quantos anos você tem?");
+    int idade = 0;
+    scanf("%i", &idade);
+    printf("Parabens, eh lindo ter %i anos!\n", idade);
+
+    puts("Qual sua altura?");
+    float altura = 0.0;
+    scanf("%f", &altura);
+    printf("Que bom, voce deve ser um xuxu com %.2f de altura\n", altura);
+
+    int dia = 0;
+    int mes = 0;
+
+    puts("Digite dia e mes de nascimento");
+    scanf("%d %d", &dia, &mes);
+    printf("blabla %d/%d", dia, mes);
+}
+
+
+
+
+
+
